ef29cffa3745973aba3c409b010b2c0368323f57
TejaswiniDudipalla <116793939+TejaswiniDudipalla@users.noreply.github.com>
2022-11-28 15:02:19 +0530

    Add files via upload

---
+++ eclipse-project/newproject/src/main/java/tdapplication/newproject/App.java
@@ -0,0 +1,18 @@
+package tdapplication.newproject;
+
+public class App {
+  public static void main(String[] args) {
+    int a=10;
+    int b=5;
+    int c=15;
+    if(a>b&&a>c) {
+    	System.out.println(a+"is greatest");
+    }
+    else if(b>c &&b>a) {
+    	System.out.println(b+"is greatest");
+    	
+    }else
+    	System.out.println(c+"is greatest");
+    	
+  }
+}
